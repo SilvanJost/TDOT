f714469077b1d41f05e81fee6f9cd75b45d0c0c3
Kevin Looser <kevin.looser@ict-campus.net>
2018-05-18 09:35:19 +0200

    Added input handling server side

---
+++ src/server/net/InputHandler.java
@@ -0,0 +1,58 @@
+package server.net;
+
+public class InputHandler {
+
+	private boolean upKeyPressed = false;
+	private boolean leftKeyPressed = false;
+	private boolean rightKeyPressed = false;
+	
+	private boolean punchKeyPressed = false;
+	private boolean throwKeyPressed = false;
+	private boolean specialKeyPressed = false;
+	
+	//set key pressed status
+	public void pressKeyUp(boolean isPressed){
+		this.upKeyPressed = isPressed;
+	}
+	public void pressKeyLeft(boolean isPressed){
+		this.leftKeyPressed = isPressed;
+	}
+	public void pressKeyRight(boolean isPressed){
+		this.rightKeyPressed = isPressed;
+	}
+	
+	public void pressKeyPunch(boolean isPressed){
+		this.punchKeyPressed = isPressed;
+	}
+	public void pressKeyThrow(boolean isPressed){
+		this.throwKeyPressed = isPressed;
+	}
+	public void pressKeySpecial(boolean isPressed){
+		this.specialKeyPressed = isPressed;
+	}
+	
+	//get key pressed status
+	public boolean isUpPressed() {
+		return upKeyPressed;
+	}
+	
+	public boolean isLeftPressed() {
+		return leftKeyPressed;
+	}
+	
+	public boolean isRightPressed() {
+		return rightKeyPressed;
+	}
+	
+	public boolean isPunchPressed() {
+		return punchKeyPressed;
+	}
+	
+	public boolean isThrowPressed() {
+		return throwKeyPressed;
+	}
+	
+	public boolean isSpecialPressed() {
+		return specialKeyPressed;
+	}
+}
